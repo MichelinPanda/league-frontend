683c73a04bd589eeba95513d6aa7983e3755a792
MichelinPanda <vedakisara88@gmail.com>
2026-05-23 13:06:44 +0100

    connect live backend

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 
-export default function Page() {
+export default async function Page() {
+  const res = await fetch(
+    "https://league-backend-w6hn.onrender.com/health",
+    { cache: "no-store" }
+  );
+
+  const data = await res.json();
+
   return (
     <div>
-      <h1>My First App 🚀</h1>
-      <p>Frontend is working!</p>
+      <h1>LIVE Frontend ↔ LIVE Backend ✅</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
-  )
+  );
 }
